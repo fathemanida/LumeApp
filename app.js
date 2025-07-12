81b2bc7b921378aadbc32be8e670f8235eac4268
fathema nida <fathemanida.009@gmail.com>
2025-07-12 22:24:56 +0530

    fixed adding product issue

--- app.js
+++ app.js
@@ -23,8 +23,9 @@ app.set("views",[
 ]);
 
 app.use(express.static(path.join(__dirname,"public")));
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 app.use(nocache());
 
 app.use(session({
